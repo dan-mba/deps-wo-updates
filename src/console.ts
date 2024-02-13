2fa45d8695399069384f518db6b7c7737788c125
Dan <35927536+dan-mba@users.noreply.github.com>
2024-02-13 08:44:35 -0500

    update console row order

--- src/console.ts
+++ src/console.ts
@@ -10,30 +10,43 @@ export function outputConsole (metadata: dependencyMetadata[] ) {
       {name: 'next'},
       {name: 'nextDate'}
     ],
-    sort: (row1, row2) => row1.package.localeCompare(row2.package)
+    disabledColumns: ['order'],
+    sort: (row1, row2) => {
+      if (row1.order === row2.order) {
+        return row1.package.localeCompare(row2.package);
+      }
+      return row2.order - row1.order;
+    }
   })
   
+  const colors = ['green', 'cyan', 'red']
   
-  const colorize = (dep: dependencyMetadata, cutoff: Date) => {
+  const order = (dep: dependencyMetadata, cutoff: Date) => {
     const d = new Date(dep.date);
     if (d > cutoff) {
-      return 'green';
+      return 0;
     }
     if (dep.nextDate) {
       const n = new Date(dep.nextDate);
       if (n > cutoff) {
-        return 'cyan';
+        return 1;
       }
     }
-    return 'red';
+    return 2;
   
   }
   
   
   let lstYear = new Date(Date.now());
-  lstYear.setFullYear(lstYear.getFullYear() - 1)
-  metadata.forEach((dep) =>  {
-    table.addRow( dep, {color: colorize(dep, lstYear)})
+  lstYear.setFullYear(lstYear.getFullYear() - 1);
+  let deps = metadata.map((dep) => {
+    return {
+      order: order(dep, lstYear),
+      ...dep
+    }
+  })
+  deps.forEach((dep) =>  {
+    table.addRow( dep, {color: colors[dep.order]})
   });
   
   table.printTable();
